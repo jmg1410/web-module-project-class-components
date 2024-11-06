b5ecaef247d15f4b9e3865d3ee689f3ce7898ee3
Jay Marmol <marmoljay@hotmail.com>
2024-11-05 22:06:17 -0500

    finished

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+  state = {
+    showAll: true
+  }
+
+  toggleshowAll = evt => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll,
+    })
+  }
   render() {
+    const { todos, toggleCompletion } = this.props
+    const filtered = todos.filter(td => !td.completed || this.state.showAll)
     return (
       <div>
-        TodoList
+        {
+          filtered.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion}/>))
+        }
+        <button onClick={this.toggleshowAll}>{this.state.showAll ? 'hide Completed Todos' : 'Show All'}</button>
       </div>
     )
   }
